383c79c3ae5c8b416d83232200d96c9bf0399a54
Nathan Bycroft <nathanbycroft@gmail.com>
2025-05-06 00:18:29 +1200

    feat: Add timeout information in appendMetadata function

--- dist/index.js
+++ dist/index.js
@@ -28930,6 +28930,7 @@ function appendMetadata(body, approvers = []) {
   metadataLines.push(`__Minimum Approvals:__ ${Core.getInput('minimumApprovals')}`);
   metadataLines.push(`__Approval Words:__ ${Core.getInput('approveWords')} (case insensitive)`);
   metadataLines.push(`__Rejection Words:__ ${Core.getInput('rejectWords')} (case insensitive)`);
+  metadataLines.push(`__Timeout:__ ${Core.getInput('waitTimeout')} minutes`);
 
   return `${body}
 
